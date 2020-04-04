82093718d78e2ccf8dd68ff3d84d1be80c71cf14
mykytam <morar.nikita@gmail.com>
2020-04-04 18:22:28 +0300

    Spring Scopes and Autowiring

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,16 +2,21 @@ import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.pluralsight"}) // scanning for Beans to autowire
 public class AppConfig {
-
+    /*
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON) // each time we ask for an instance of Bean from the ApplicationContext, it's returning the same Bean
     public SpeakerService getSpeakerService() {
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
-        // repository is now injected through constructor injection into SpeakerServiceImpl
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        // repository is now injected through Autowiring ...constructor injection into SpeakerServiceImpl
         // we can call getSpeakerRepository a lot of times and only 1 of SpeakerServiceImpls will be created
         return service;
     }
@@ -20,5 +25,5 @@ public class AppConfig {
     public SpeakerRepository getSpeakerRepository() {
         return new HibernateSpeakerRepositoryImpl();
     }
-
+    */
 }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -2,13 +2,23 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository; // SpeakerServiceImpl now injected, not hardcoded
 
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
+    @Autowired
     public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        System.out.println("SpeakerServiceImpl repository constructor");
         repository = speakerRepository;
     }
 
@@ -17,4 +27,8 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
+    public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
+        this.repository = repository;
+    }
 }
