5f7d5cb78cded46266b9e61ad2f489593ac85e9f
mykytam <morar.nikita@gmail.com>
2020-04-06 08:47:29 +0300

    Rethinking the project. New template

---
+++ src/main/java/com/pluralsight/ConferenceApplication.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceApplication.class);
+    }
+}
